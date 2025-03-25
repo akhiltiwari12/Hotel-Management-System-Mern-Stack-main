9f5838df36a4a05f3c72b546f782739290f755f4
AKHIL KUMAR TIWARI <159538883+akhiltiwari12@users.noreply.github.com>
2025-03-25 18:25:19 +0530

    home page modifiend2

--- frontend/src/components/auth/cusLogin.js
+++ frontend/src/components/auth/cusLogin.js
@@ -36,7 +36,7 @@ export default function CusLogin() {
             <div class="container-fluid h-custom">
                 <div class="row d-flex justify-content-center align-items-center h-100">
                     <div class="col-md-9 col-lg-6 col-xl-5">
-                        <img src="https://www.linkpicture.com/q/5312964.jpg"
+                        <img src="https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             class="img-fluid" alt="Sample image" />
                     </div>
                     <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
